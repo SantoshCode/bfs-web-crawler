853beab2ba20063563e78fdf19c2b3278e48604b
Santosh Subedi <santsubcode@gmail.com>
2022-04-10 21:17:23 +0545

    init;

---
+++ levelCrawler.js
@@ -0,0 +1,36 @@
+const { headLineCrawler } = require('./headlineCrawler')
+const { categoryCrawler } = require('./categoryCrawler')
+
+const levelCrawler = async ({level, href}) => {
+	if (level === 3) {
+        const _data = (await headLineCrawler(href)).slice(0, 5)
+        return _data.map(item => ({category: item.category, title: item.title, level: 4}))
+    }
+
+	if (level === 0) return [{
+        href: 'https://bbc.com',
+        level: 1
+    }]
+
+	if (level === 1) {
+        const _data = (
+			await categoryCrawler(
+				href,
+				'ul > li > a[href^="https://www.bbc.com/"]'
+			)
+		).slice(0, 3)
+        return _data.map(item => ({href: item.href, level: 2}))
+    }
+
+
+	if (level === 2){
+        const _data = (await categoryCrawler(href, `a[href^="/"]`)).slice(
+			1,
+			4
+		)
+
+        return _data.map(item => ({href: item.href, level: 3}))
+    }
+}
+
+module.exports = { levelCrawler }
